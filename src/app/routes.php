f34f4fae3b5a3d40b262da51fde7c0aa33d8c09c
eogml0716 <57823242+eogml0716@users.noreply.github.com>
2022-01-27 06:19:28 +0900

    게시글 관련 작업 중
    Comment : 2022-01-26 작업 커밋

--- src/app/routes.php
+++ src/app/routes.php
@@ -6,12 +6,27 @@ use MyLifeServer\core\Router;
 // 사용자가 사이트에 접속했을 때 Router 객체를 생성해줌
 $router = new Router();
 
-/** ------------ @category 1. 사용자 ------------ */
+/** ------------ @category 1. 유저 ------------ */
 // 회원가입
 $router->post('/signup', 'User@signup');
 
-// 로그인, 세션, 로그아웃 관련
+// 로그인, TODO: 자동 로그인
 $router->post('/signin/:type(general|auto)', 'User@signin');
+// TODO: 로그아웃
 $router->delete('/signout', 'User@signout');
+
+/** ------------ @category 2. 게시글, 댓글 ------------ */
+// TODO: 게시글, 댓글 리스트 가져오기 (무한 스크롤링)
+$router->get('/read/:type(posts|comments)', 'Board@read');
+// TODO: 게시글, 댓글 추가
+$router->post('/create/:type(post|comment)', 'Board@create');
+// TODO: 게시글, 댓글 수정
+$router->put('/update/:type(post|comment)', 'Board@update');
+// TODO: 게시글, 댓글 삭제
+$router->delete('/delete/:type(post|comment)', 'Board@delete');
+// TODO: 게시글, 댓글 좋아요
+$router->post('/create/like/:type(post|comment)', 'Board@create_like');
+
+/** ------------ @category 4. 마이페이지 ------------ */
 
 return $router; // 경로를 추가한 라우터 객체 반환
